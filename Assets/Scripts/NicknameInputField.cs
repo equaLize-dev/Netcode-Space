11dbcad2628981ae2938cf9dc28450462f87a947
equaLize <85683486+equaLize-dev@users.noreply.github.com>
2022-08-20 13:29:39 +0300

    Fixed player name sycronizing feature.

--- Assets/Scripts/NicknameInputField.cs
+++ Assets/Scripts/NicknameInputField.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using TMPro;
-using Unity.Netcode;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NicknameInputField : Singleton<NicknameInputField>
+public class NicknameInputField : MonoBehaviour
 {
-    public event Action<string> OnNicknameAvailable = delegate {  };
-    
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private Button continueButton;
     [SerializeField] private int minCharacters;
     [SerializeField] private int maxCharacters;
-    private NetworkVariable<NetworkString> _nickname = new ();
-    private string _oldNickname;
-
-    public override void OnNetworkSpawn()
-    {
-        if (IsClient && IsOwner)
-        {
-            UpdateClientNicknameServerRpc(inputField.text);
-            OnNicknameAvailable.Invoke(_nickname.Value);
-        }
-    }
 
     private void Start()
     {
@@ -41,11 +26,5 @@ public class NicknameInputField : Singleton<NicknameInputField>
             
             continueButton.interactable = true;
         } );
-    }
-
-    [ServerRpc]
-    private void UpdateClientNicknameServerRpc(string clientNickname)
-    { 
-        _nickname.Value = clientNickname;
     }
 }
--- Assets/Scripts/PlayerHud.cs
+++ Assets/Scripts/PlayerHud.cs
@@ -1,34 +1,51 @@
 ﻿using TMPro;
 using Unity.Netcode;
+using UnityEngine;
 
 public class PlayerHud : NetworkBehaviour
 {
      private NetworkVariable<NetworkString> _playerName = new();
+     private TMP_InputField _inputField;
      private bool overlaySet;
 
      public override void OnNetworkSpawn()
      {
+          _inputField = FindObjectOfType<TMP_InputField>();
+          
           if (IsServer)
           {
-               NicknameInputField.Instance.OnNicknameAvailable += delegate (string nickname)
+               if (!string.IsNullOrEmpty(_playerName.Value))
                {
-                    _playerName.Value = nickname;
-               };
+                    SetOverlay(_playerName.Value);
+                    overlaySet = true;
+               }
+          }
+          
+          if (IsClient && IsOwner)
+          {
+               UpdateClientNicknameServerRpc(_inputField.text);
+               SetOverlay(_inputField.text);
           }
      }
 
-     public void SetOverlay()
+     public void SetOverlay(string text)
      {
           var localPlayerOverlay = gameObject.GetComponentInChildren<TextMeshProUGUI>();
-          localPlayerOverlay.text = _playerName.Value;
+          localPlayerOverlay.text = text;
      }
 
      private void Update()
      {
           if (!overlaySet && !string.IsNullOrEmpty(_playerName.Value))
           {
-               SetOverlay();
+               SetOverlay(_playerName.Value);
                overlaySet = true;
           }
+     }
+
+     [ServerRpc]
+     public void UpdateClientNicknameServerRpc(string nickname)
+     {
+          _playerName.Value = nickname;
      }
 }
