5d7e1d72fab7c45a05f12aab5534cbe26354c7d3
equaLize <85683486+equaLize-dev@users.noreply.github.com>
2022-08-21 15:52:10 +0300

    Update PlayerControl.cs (without animation support)

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,70 +1,105 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerControl : NetworkBehaviour
 {
-    [SerializeField] private float walkSpeed = 3.5f;
+    [SerializeField] private float speed = 3.5f;
+    [SerializeField] private float rotationSpeed = 1.5f;
+    [SerializeField] private NetworkVariable<PlayerState> networkPlayerState = new();
+    [SerializeField] private NetworkVariable<Vector3> networkPositionDirection = new();
+    //[SerializeField] private NetworkVariable<Vector3> networkRotationDirection = new();
     [SerializeField] private NetworkVariable<float> forwardBackPosition = new();
     [SerializeField] private NetworkVariable<float> leftRightPosition = new();
+
+    private CharacterController _controller;
+    private Animator _animator;
+    private Vector3 _direction;
     
     // client caching
-    private float _oldForwardBackPosition;
-    private float _oldleftRightBackPosition;
+    private Vector3 _oldInputPosition;
+    //private Vector3 _oldInputRotation;
+
+    private static readonly int s_VerticalMove = Animator.StringToHash("VerticalMove");
+    private static readonly int s_Idle = Animator.StringToHash("Idle");
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
-        if (IsServer)
-        {
-            UpdateServer();
-        }
-
         if (IsClient && IsOwner)
         {
-            UpdateClient();
+            ClientInput();
+        }
+        
+        ClientMoveAndRotate();
+        ClientVisuals();
+    }
+
+    private void ClientInput()
+    {
+        // player position and rotation input
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+
+        if (_oldInputPosition != _direction)
+        {
+            _oldInputPosition = _direction;
+            UpdateClientPositionServerRpc(_direction * speed);
+        }
+        
+        // player state changes based on input
+        if (_direction.x != 0 || _direction.z != 0)
+        {
+            UpdatePlayerStateServerRpc(PlayerState.Move);
+        }
+        
+        else
+        {
+            UpdatePlayerStateServerRpc(PlayerState.Idle);
         }
     }
 
-    private void UpdateServer()
+    private void ClientMoveAndRotate()
     {
-        transform.position = new Vector3(transform.position.x + leftRightPosition.Value, 
-            transform.position.y, transform.position.z + forwardBackPosition.Value);
-    }
-
-    private void UpdateClient()
-    {
-        float forwardBackward = 0;
-        float leftRight = 0;
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (networkPositionDirection.Value != Vector3.zero)
         {
-            forwardBackward += walkSpeed;
-        }        
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            forwardBackward -= walkSpeed;
-        }        
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            leftRight -= walkSpeed;
-        }        
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            leftRight += walkSpeed;
+            _controller.Move(networkPositionDirection.Value * Time.deltaTime);
         }
-
-        if (_oldForwardBackPosition != forwardBackward || _oldleftRightBackPosition != leftRight)
+    }    
+    
+    private void ClientVisuals()
+    {
+        if (networkPlayerState.Value == PlayerState.Move)
         {
-            _oldForwardBackPosition = forwardBackward;
-            _oldleftRightBackPosition = leftRight;
-            //update the server
-            UpdateClientPositionServerRpc(forwardBackward, leftRight);
+            _animator.SetFloat(s_VerticalMove, 1);
+        }
+        
+        else if (networkPlayerState.Value == PlayerState.Idle)
+        {
+            _animator.SetFloat(s_VerticalMove, 0);
         }
     }
 
     [ServerRpc]
-    private void UpdateClientPositionServerRpc(float forwardBackward, float leftRight)
+    private void UpdateClientPositionServerRpc(Vector3 newPositionDirection)
     {
-        forwardBackPosition.Value = forwardBackward;
-        leftRightPosition.Value = leftRight;
+        networkPositionDirection.Value = newPositionDirection;
     }
+
+    [ServerRpc]
+    public void UpdatePlayerStateServerRpc(PlayerState newState)
+    {
+        networkPlayerState.Value = newState;
+    }
+}
+
+public enum PlayerState
+{ 
+    Idle, 
+    Move
 }
