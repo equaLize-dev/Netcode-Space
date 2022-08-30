56d199613e5f1038953c5f775122b4feefaf00be
equaLize <85683486+equaLize-dev@users.noreply.github.com>
2022-08-30 11:49:55 +0300

    Better camera controller.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,16 +4,11 @@ public sealed class CameraControl : MonoBehaviour
 {
     [SerializeField, Min(0)] private float sensitivity;
     [SerializeField, Min(0)] private float bodyFollowingIntensity;
-    [SerializeField, Range(-90.0f, 0.0f)] private float minVerticalAngle;
-    [SerializeField, Range(0.0f, 90.0f)] private float maxVerticalAngle;
 
     private Quaternion _rotation;
     private Transform _parent;
 
     public Transform Parent { set => _parent = value; }
-    
-    private bool KeyboardInput => 
-        Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
 
     private void Start()
     {
@@ -22,25 +17,23 @@ public sealed class CameraControl : MonoBehaviour
 
     private void Update()
     {
-        transform.position = 
-            Vector3.Lerp(transform.position, _parent.position, Time.deltaTime * bodyFollowingIntensity);
+        if (_parent != null)
+        {
+            transform.position = 
+                Vector3.Lerp(transform.position, _parent.position, Time.deltaTime * bodyFollowingIntensity);
+        }
         
-        if (!KeyboardInput)
+        if (!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))
         {
             _rotation.x -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-            _rotation.y += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-            _rotation.x = Mathf.Clamp(_rotation.x, minVerticalAngle, maxVerticalAngle);
-            transform.rotation = Quaternion.Euler(_rotation.x, _rotation.y, 0);
         }
 
         else
         {
-            if (_parent != null)
-            {
-                _rotation.x += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-                _rotation.y += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-                transform.rotation = Quaternion.Euler(_rotation.x, _rotation.y, 0);
-            }
+            _rotation.x += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
         }
+        
+        _rotation.y += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(_rotation.x, _rotation.y, 0);
     }
 }
